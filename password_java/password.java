6788826367b8a20159282cdd142e638885bc48c3
Kepilyn V <80963070+NylipekWolf@users.noreply.github.com>
2021-07-16 15:05:43 -0300

    Add files via upload

---
+++ password_java/password.java
@@ -0,0 +1,5 @@
+public class password() {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ password_java/password/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+import javax.annotation.processing.Generated;
+public class App {
+    public static void main(String[] args){
+        int lenght = 10; // password lenght
+        System.out.println(generatePswd(lenght));
+    }
+    static char[] generatePswd(int len){
+        System.out.println("Sua Senha: ");
+        String charsCaps = "ABCDFGHIJKLMNOPQRSTUVWYZ";
+        String chars = "abcdefghiklmnopqrstuvwxyz";
+        String numeros = "0123456789";
+        String symbols = "!@#$%¨&*()_/.<>";
+        String passSymbols = charsCaps + chars + nums + symbols;
+        Random rnd = new Random();
+        char[] password = new char[len];
+        for(int i = 0; i < len; i++){
+            password[i] = passSymbols.charAt(rnd.nextInt(passSymbols.length()));
+        }
+        return password;
+    }
+}
